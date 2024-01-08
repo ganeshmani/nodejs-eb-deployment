aa927939491f445463f375de3c35975c5cecabd5
Ganesh Mani <ganesh.mani@mercanis.com>
2024-01-08 21:56:56 +0530

    fix: connect db

--- src/db-config.ts
+++ src/db-config.ts
@@ -6,18 +6,31 @@ import { Movie } from './entites/Movies.entity';
 
 dotenv.config();
 
+let dbHost = process.env.DB_HOST;
+let dbPort = parseInt(process.env.DB_PORT || '5432');
+let dbUsername = process.env.DB_USERNAME;
+let dbPassword = process.env.DB_PASSWORD;
+let dbDatabase = process.env.DB_DATABASE;
+
 const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, NODE_ENV } =
   process.env;
 
+if (NODE_ENV !== 'dev') {
+  dbHost = process.env.RDS_HOSTNAME;
+  dbPort = parseInt(process.env.RDS_PORT || '5432');
+  dbDatabase = process.env.RDS_DB_NAME;
+  dbUsername = process.env.RDS_USERNAME;
+  dbPassword = process.env.RDS_PASSWORD;
+}
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: DB_HOST,
-  port: parseInt(DB_PORT || '5436'),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-
-  synchronize: NODE_ENV === 'dev' ? false : false,
+  host: dbHost,
+  port: dbPort,
+  username: dbUsername,
+  password: dbPassword,
+  database: dbDatabase,
+  synchronize: NODE_ENV === 'dev' ? false : true,
   //logging logs sql command on the treminal
   logging: NODE_ENV === 'dev' ? false : false,
   entities: [Movie],
